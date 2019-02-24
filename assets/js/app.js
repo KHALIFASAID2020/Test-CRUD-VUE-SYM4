73225e35549f175cee9f16d23e69c8e2931ab298
KHALIFA SAID <khalifasaid.2020@gmail.com>
2019-02-24 17:11:44 +0100

    Initialisation Projet+FOSRestFull dependency

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import App from './component/App';
+
+const app = new Vue({
+    el: '#app',
+    render: h => h(App)
+});
---
+++ assets/js/component/App.vue
@@ -0,0 +1,16 @@
+<template>
+   <!-- Your HTML tags-->
+   <h2 class="center"> Vue Js Project </h2>
+</template>
+
+<script>
+   // Your JavaScript code
+  console.log("Loaded")
+</script>
+
+<style>
+  /* Your styles */
+  .center {
+     Text-align: center;
+  }
+</style>
